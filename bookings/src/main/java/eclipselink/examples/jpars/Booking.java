df0d2d9cdcf99ca802bd5cbdcfd44c84e34e147f
Gul Onural <gul.onural@oracle.com>
2013-02-28 11:38:03 -0500

    nitial checkin of JPARS example booking shell

---
+++ bookings/src/main/java/eclipselink/examples/jpars/Booking.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package main.java.eclipselink.examples.jpars;
+
+/**
+ * @author gonural
+ *
+ */
+public class Booking {
+
+	/**
+	 * 
+	 */
+	public Booking() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
