02a82efcaaf150745bd777c78470d25fe204fd72
Shaun Smith <shaun.smith@oracle.com>
2013-04-05 11:42:31 -0400

    Remove unnecessary scripts.

--- polyglot/client/cleandatabases.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./runclass.sh example.CleanDatabases
--- polyglot/client/runclass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-CLASSPATH=
-for i in `ls target/*.jar`
-do
-CLASSPATH=${CLASSPATH}:${i}
-done
-java -classpath ${CLASSPATH} $1
